c7a6ffd65f889604d4a2a2da3108b1ead0d23d55
NandaKishor03 <nandakishor6772@gmail.com>
2025-03-27 22:24:20 +0530

    Complete UI Changes
    Complete UI Changes

--- todolist/src/components/Create.jsx
+++ todolist/src/components/Create.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import DueDate from "./DueDate";
+import "./Create.css";
 
 function Create({ setTodos }) {
   const [task, setTask] = useState("");
