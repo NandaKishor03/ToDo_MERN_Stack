aa5e97e10c43ef6c729113e12c9c7e88819ef00b
NandaKishor03 <nandakishor6772@gmail.com>
2025-03-29 15:07:40 +0530

    UI changes
    UI changes

--- todolist/src/components/Home.jsx
+++ todolist/src/components/Home.jsx
@@ -103,7 +103,7 @@ function Home() {
 
   const renderTasks = (tasks) =>
     tasks.length === 0 ? (
-      <p>No tasks available</p>
+      <p className="no-task">No tasks available</p>
     ) : (
       tasks.map((todo) => (
         <div
@@ -117,11 +117,11 @@ function Home() {
               checked={todo.done || false}
               onChange={() => handleComplete(todo._id)}
             />
-            <div>
+            {/* <div>
               <h3>
                 {todo.task} <span className="task-date">{todo.dueDate}</span>
               </h3>
-            </div>
+            </div> */}
           </div>
           <button
             className="delete-icon"
